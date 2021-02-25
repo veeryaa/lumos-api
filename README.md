f4ab18a87f8b3a93b313374d1070d68110bab34a
Fadel Firmansyah <fadelfirmansyah@pop-os.localdomain>
2021-02-25 11:49:10 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# LUMOS REST - API 
---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.titl
+  res.status(200).json({
+    status: 200,
+  });
+});
+
+app.listen(1997, '127.0.0.1', () => {
+  console.log('Server is listening to port 1997');
+});
