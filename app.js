e4107887cebfc3d0e2e7aee9fd2008c9a3f6ae52
Fadel Firmansyah <fadelfirmansyah@pop-os.localdomain>
2021-02-28 10:44:00 +0700

    added db sync

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+'use strict';
+
 const express = require('express');
+const cors = require('cors');
+const db = require('./models/index').sequelize;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.status(200).json({
@@ -10,6 +15,11 @@ app.get('/', (req, res) => {
   });
 });
 
-app.listen(1997, '127.0.0.1', () => {
-  console.log('Server is listening to port 1997');
-});
+db.sync()
+  .then((e) => {
+    app.listen(1997, () => {
+      console.log('Server is listening to port 1997.');
+    });
+    console.log('Database is synchronized.');
+  })
+  .catch((err) => console.log(err));
